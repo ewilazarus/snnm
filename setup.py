9ee05e925b71b11be9684186a66ee92dee06848b
Gabriel Lima <ewilazarus@gmail.com>
2015-11-09 02:00:17 +0100

    New version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='snnm',
-      version='0.1.1',
+      version='0.1.2',
       author='Gabriel Lima',
       author_email='ewilazarus@gmail.com',
       description='Naming aid',
@@ -10,7 +10,7 @@ setup(name='snnm',
       url='https://github.com/ewilazarus/snnm',
       packages=['snnm'],
       install_requires=['beautifulsoup4',],
-      download_url='https://github.com/ewilazarus/snnm/tarball/0.1.1',
+      download_url='https://github.com/ewilazarus/snnm/tarball/0.1.2',
       entry_points={
           'console_scripts': [
               'snnm = snnm.cli:main'
