8d86b31926b294e79563a394139a34b14e684f81
Gabriel Lima <gvclima@gmail.com>
2017-09-03 12:00:46 -0300

    v0.2.9 publication

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = '0.2.8'
+VERSION = '0.2.9'
 
 setup(name='snnm',
       version=VERSION,
@@ -11,11 +11,12 @@ setup(name='snnm',
       keywords='synonym',
       url='https://github.com/ewilazarus/snnm',
       download_url='https://github.com/ewilazarus/snnm/tarball/' + VERSION,
+      py_modules=['snnm'],
       install_requires=[
           'beautifulsoup4==4.6.0',
           'click==6.7',
           'requests==2.18.4'
       ],
       entry_points={
-          'console_scripts': ['snnm=snnm.py:main']
+          'console_scripts': ['snnm=snnm:main']
       })
