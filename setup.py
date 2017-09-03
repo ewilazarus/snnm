b9e399efd54f68ff3069100a52553eca5582c717
Gabriel Lima <gvclima@gmail.com>
2017-09-03 10:40:41 -0300

    v0.2.0 publication

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='snnm',
+      version='0.2.0',
+      author='Gabriel Lima',
+      author_email='gvclima@gmail.com',
+      description='Naming helper',
+      license='MIT',
+      keywords='synonym',
+      url='https://github.com/ewilazarus/snnm',
+      packages=['snnm'],
+      download_url='https://github.com/ewilazarus/snnm/tarball/0.2.0',
+      entry_points={'console_scripts': ['snnm = snnm']})
