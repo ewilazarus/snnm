ec2e71f8cbb804c5374052afec3d8913316d12d7
Gabriel Lima <gvclima@gmail.com>
2017-09-03 11:48:38 -0300

    v0.2.5 publication

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 from setuptools import setup
 
+VERSION = '0.2.5'
+
 setup(name='snnm',
-      version='0.2.4',
+      version=VERSION,
       author='Gabriel Lima',
       author_email='gvclima@gmail.com',
       description='Naming helper',
       license='MIT',
       keywords='synonym',
       url='https://github.com/ewilazarus/snnm',
-      download_url='https://github.com/ewilazarus/snnm/tarball/0.2.4',
+      download_url='https://github.com/ewilazarus/snnm/tarball/' + VERSION,
       install_requires=[
           'beautifulsoup4==4.6.0',
           'click==6.7',
           'requests==2.18.4'
       ],
       entry_points={
-          'console_scripts': ['snnm=snnm:main']
+          'console_scripts': ['snnm=snnm.py:main']
       })
